43ef9348badf94cbea53b2535302dc6e17cd5a85
Lantianyou <dtlantianyou@gmail.com>
2019-12-08 14:33:23 +0800

    1

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => (
+    <div>
+        <div className="bar">
+            <a href=""></a>
+        </div>
+        <div className="sub-bar">
+
+        </div>
+        <div>Cart</div>
+    </div>
+)
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Sell = props => (
+  <div>
+    <h1>Hello World</h1>
+    <Link href="/">
+      <a>home</a>
+    </Link>
+  </div>
+)
+
+export default Sell
